f4cef0cb4842027b725c97c1f7c534e10baa4f69
Gonzalo Guimaraens <gonzaguima@gmail.com>
2019-06-23 22:20:13 -0300

    web y dominio

---
+++ SIstemaViviendas/SIstemaViviendas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SIstemaViviendas.Startup))]
+namespace SIstemaViviendas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
