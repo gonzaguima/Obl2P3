215028533e6d142e6ad58061a0484ccb5aec62a0
pingold16 <p.ingold@outlook.com>
2019-06-27 20:29:12 -0300

    as

--- SIstemaViviendas/Dominio/Clases/Barrio.cs
+++ SIstemaViviendas/Dominio/Clases/Barrio.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Dominio.Models
 {
